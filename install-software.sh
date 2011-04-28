a3a145c23cec5046e83a56800ff5b4f9a6ceee6c
Sam Britt <samuelbritt@gmail.com>
2011-04-28 12:23:53 -0400

    converted sofware list to an install script

---
+++ install-software.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# install these first so we can start doing work
+sudo apt-get install 			\
+	vim-gnome			\
+	mercurial			\
+
+# now the rest
+sudo apt-get install 			\
+	ipython                         \
+	python-scipy                    \
+	python-matplotlib               \
+	inkscape                        \
+	scribus-ng                      \
+	scribus-ng-doc                  \
+	gimp                            \
+	gnome-do                        \
+	vlc                             \
+	htop                            \
+	tree                            \
+	nautilus-open-terminal          \
+	engauge-digitizer               \
+	compizconfig-settings-manager   \
+	qalculate-gtk                   \
+	openssh-server                  \
+	electricsheep                   \
+	network-manager-vpnc            \
+	exuberant-ctags                 \
+	libdvdcss2			\
+
+	## Others?
+	# chromium-browser		\
+	# backintime-gnome		\
+	# deja-dup			\
+
+# Install LaTeX last b/c it takes forever
+sudo apt-get install 			\
+	texlive				\
+	texlive-bibtex-extra            \
+	texlive-math-extra              \
+	texlive-publishers              \
+	texlive-science                 \
+
+
+echo
+echo "Now that the install is done, be sure to install these from the web:"
+echo "Virtualbox (http://www.virtualbox.org/wiki/Linux_Downloads)"
+echo "Chrome (http://www.google.com/chrome)"
+echo "Eclipse (http://eclipse.org/downloads/)"
+echo "EPD (http://www.enthought.com/products/edudownload.php)"
