07141e317638c1367740b6d13fc0ad40e22041e8
Sam Britt <samuelbritt@gmail.com>
2012-12-13 18:58:18 -0500

    update the backup script (work in progress)

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-
 # Taken from http://blog.interlinked.org/tutorials/rsync_time_machine.html
 
+# To set up as a cron job, first edit the cron table (as root):
+#	$ crontab -e
+# Then add the following line:
+#	20 4 * * * ~/etc/backup/backup.sh
+# to run every morning at 4:20 AM.
+
 # Edit these
-SRCDIR=test
 DESTHOST=hydrogen.local
 DESTDIR=/media/Paris/bak
+
+# SRCDIR=$HOME
+INCLUDES=$HOME/etc/backup/include
+EXCLUDES=$HOME/etc/backup/exclude
 
 # Do not edit below
 SRCHOST=${HOSTNAME%.local}
@@ -15,12 +23,22 @@ date=`date "+%Y-%m-%d-%H%M%S"`
 
 ssh $DESTHOST "mkdir -p $BACKUPPATH"
 
-rsync \
-	-azP \
-	--delete \
-	--delete-excluded \
-	--link-dest=$BACKUPPATH/current \
-	$SRCDIR $DESTHOST:$BACKUPPATH/$date
+for SRCDIR in `cat $INCLUDES`
+do
+	echo "Backing up $SRCDIR to $DESTHOST:$BACKUPPATH"
+		# --copy-links \
+	rsync \
+		--dry-run \
+		--archive \
+		--compress \
+		--partial \
+		--progress \
+		--delete \
+		--delete-excluded \
+		--exclude-from=$EXCLUDES \
+		--link-dest=$BACKUPPATH/current \
+		$SRCDIR $DESTHOST:$BACKUPPATH/$date
+done
 
 ssh $DESTHOST "\
 	rm -f $BACKUPPATH/current \
