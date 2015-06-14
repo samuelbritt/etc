6a848d82ebda0ed1e8bb1b59f07cf8b3730d85ff
Sam Britt <samuelbritt@gmail.com>
2015-06-14 10:09:33 -0400

    refactor colors

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,10 +1,3 @@
-color=0;
-
-while [ $color -lt 256 ]; do
-    echo -e "$color: \e[38;5;${color}mhello\e[48;5;${color}mworld\e[0m"
-    ((color++));
-done
-
 
 T='gYw'   # The test text
 echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m";
@@ -17,3 +10,49 @@ for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33
     echo;
 done
 echo
+
+
+function fg {
+	echo "\e[38;5;${1}m"
+}
+
+function bg {
+	echo "\e[48;5;${1}m"
+}
+
+function colorblock {
+	startcolor=$1
+	endcolor=$2
+	cols=$3
+
+	rows=$(( ($endcolor - $startcolor) / $cols ))
+	color=$startcolor
+	r=0
+	while [[ $r -lt $rows ]]; do
+		c=0
+		row=""
+		color=$(( $startcolor + $r ))
+		while [[ $c -lt $cols  ]]; do
+			fgc=$(fg $color)
+			bgc=$(bg $color)
+			printf -v row "$row\t%3d ${fgc}hello${bgc}world\e[0m"  $color;
+			color=$(($color + $rows));
+			((c++));
+		done
+		echo "$row"
+		((r++));
+	done
+}
+
+echo
+echo "ANSI 16"
+colorblock 0 16 8
+
+
+echo
+echo "ANSI 256 Grayscale"
+colorblock 232 256 8
+
+echo
+echo "ANSI 256"
+colorblock 16 232 6
