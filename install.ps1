7da744a7876d5ad390a811451044f49fa258715b
Samuel Britt <samuelbritt@gmail.com>
2018-03-11 14:44:20 -0400

    add chocolatey installer

---
+++ install.ps1
@@ -0,0 +1,24 @@
+Set-ExecutionPolicy RemoteSigned -Force
+
+# Chocolatey
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Push-Location $PSScriptRoot
+
+# Chocolatey packages
+choco install ./apps/windows-packages.config -y
+
+# Powershell modules
+Install-Module Posh-Git
+
+# Profile
+$profileParams = @{
+    Path = "${env:HOME}\Documents\WindowsPowerShell\profile.ps1"
+    Value = "$PSScriptRoot\powershell\profile.ps1"
+    ItemType = "SymbolicLink"
+}
+New-Item @profileParams
+
+Pop-Location
